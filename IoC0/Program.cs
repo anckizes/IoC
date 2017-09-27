b7d8f521223d59917ccebade5f11d6633f842acd
Diego Anckizes <diegoanckizes@gmail.com>
2017-09-27 10:31:14 -0300

    :sparkles: Adiciona logica sem injecao de dependencia

--- IoC0/Program.cs
+++ IoC0/Program.cs
@@ -6,7 +6,69 @@ namespace IoC0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Database database = new Database();
+            FileWriter fileWriter = new FileWriter();
+            MailNotification mailNotification = new MailNotification();
+            PidgeonNotification pidgeonNotification = new PidgeonNotification();
+            bool isABadThingUsingAnimalsForSendingMessages = true;
+
+            database.Select();
+            fileWriter.Write();
+            database.UpdateChanges();
+            if (isABadThingUsingAnimalsForSendingMessages)
+            {
+                mailNotification.SendNotification();
+            }
+            else
+            {
+                pidgeonNotification.SendNotification();
+            }
+
+
         }
+    }
+
+    public class Database
+    {
+        public void UpdateChanges()
+        {
+            Console.WriteLine("Bz-bzz. Data updated!");
+        }
+
+        public void Select()
+        {
+            Console.WriteLine("Bbbzbzbzbz-zz. Data loaded!");
+        }
+    }
+
+    public class FileWriter
+    {
+        public void Write()
+        {
+            Console.WriteLine("Bz-booom. File saved!");
+        }
+    }
+
+    public interface Notification
+    {
+        void SendNotification();
+    }
+
+    public class MailNotification : Notification
+    {
+        public void SendNotification()
+        {
+            Console.WriteLine("Bz Mail Sent bzzzz!");
+        }
+
+    }
+
+    public class PidgeonNotification : Notification
+    {
+        public void SendNotification()
+        {
+            Console.WriteLine("Bz Pombo Sent bzzzz!");
+        }
+
     }
 }
