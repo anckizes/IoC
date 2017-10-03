9788ddac2a8bc05af69de5e54d857138cdd12e97
Diego Anckizes <diegoanckizes@gmail.com>
2017-10-03 09:55:14 -0300

    Adiciona IoC Setter

---
+++ IoC2Setter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoC2Setter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
